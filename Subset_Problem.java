95a9b14dc04a5293511e4fd469697658d7784310
Shivvy-The-Coder <krshivam455@gmail.com>
2024-09-22 23:49:26 +0530

    vivek loves array game code has been optimized and added

--- Subset_Problem.java
+++ Subset_Problem.java
@@ -3,7 +3,7 @@ public class Subset_Problem{
     static int count =0;
     public static void main(String args[]) 
     {
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
         ArrayList <Integer> ll = new ArrayList<>();
         ll.add(1);
         ll.add(2);
